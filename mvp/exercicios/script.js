7a7ee29285a5a8e2452ed723475a7eb159eed8ed
Alexandre <alexandre.bueno0204@gmail.com>
2024-08-08 15:32:22 -0300

    new features on  exercises page

--- mvp/exercicios/script.js
+++ mvp/exercicios/script.js
@@ -8,21 +8,24 @@ let exerciciosCompletos = 0; // let serve para definir variavel que pode ter seu
                              // nesse caso o numero de exercicios completos q mudara com o tempo
 
 // info dos exerciciios
-// aq tamo definindo um objeto(const) exercicios que tem a chave de cada ex(1, 2, 3, respectivos titulos e conteudos
+// aq tamo definindo um objeto(const) exercicios que tem a chave de cada ex(1, 2, 3, respectivos titulos, conteudos, resposta, status etc
 const exercicios = {
     1: {
         titulo: '1. Soma de dois valores.', // titulo
-        conteudo: 'Nesse exercicio voce deve calcular o valor da soma de dois numeros.', // desc
+        conteudo: 'Quanto é 10 + 10?', // desc
+        respostaCorreta: "20",
         completo: false // status do ex
     },
     2: {
         titulo: '2. Subtracao de dois valores.',
-        conteudo: 'Nesse exercicio voce deve calcular o valor da subtracao de dois numeros.',
+        conteudo: 'Quanto é 10 - 5?',
+        respostaCorreta: "5",
         completo: false
     },
     3: {
         titulo: '3. Multiplicacao de dois valores',
-        conteudo: 'Nesse exercicio voce deve calcular o valor da multiplicacao de dois numeros.',
+        conteudo: 'Quanto é 10 x 5?',
+        respostaCorreta: "50",
         completo: false
     }
 };
@@ -38,16 +41,23 @@ listaDeExercicios.addEventListener('click', function(evento) {
         areaDeConteudoExercicio.innerHTML = `
             <h3>${exercicio.titulo}</h3>
             <p>${exercicio.conteudo}</p>
-            <button id="enviar-exercicio">Enviar</button>
+            <textarea id="resposta-exercicio-${id}" class="input-answer" rows="4" cols="50"></textarea><br>
+            <button id="enviar-exercicio" class="button-submit-exercise">Submit</button>
         `; // esse ${} e so p colocar variavel
 
     const botaoEnviar = document.getElementById('enviar-exercicio');
     botaoEnviar.addEventListener('click', function() {
+        const respostaUsuario = document.getElementById(`resposta-exercicio-${id}`).value;
 
-        if (!exercicio.completo) {
-            exercicio.completo = true;
-            exerciciosCompletos++;
-            atualizarProgresso();
+        if (respostaUsuario.trim() === exercicio.respostaCorreta) { // verfiica se a resposta ta certa
+            if (!exercicio.completo) {
+                exercicio.completo = true;
+                exerciciosCompletos++;
+                atualizarProgresso();
+                alert("Resposta Correta.") // deixar isso mais user friendly
+            }
+        } else {
+            alert("Resposta incorreta. Tente novamente.");
         }
     });
 }
@@ -58,7 +68,3 @@ function atualizarProgresso(){
     barraDeProgresso.style.width = `${porcentagem}%`;
     textoDeProgresso.textContent = `${exerciciosCompletos} / 3 completos`;
 }
-
-
-// PASSAR BOTAO PARA HTML PARA CUSTOMIZAR COM CSS
-// FEITO AQUI SOMENTE COMO TESTE
